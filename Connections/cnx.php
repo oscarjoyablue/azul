6c42e926898d935b30d6b74bc1dcb6523f69294f
oscarjoyablue <opalacios@bancoazul.com>
2022-11-15 08:54:31 -0600

    CambioDB
    Cambio de Cadena de Conexion

--- Connections/cnx.php
+++ Connections/cnx.php
@@ -1,7 +1,7 @@
 <?php
-	$hostname = 'localhost';
-	$username = 'root';
-	$password = '';
+	$hostname = 'bancoazulbd.database.windows.net';
+	$username = 'Administrador';
+	$password = 'Welcome.2020';
 	$database_cnx = 'elecciones2020';
 	$cnx = mysqli_connect($hostname, $username, $password, $database_cnx) or trigger_error("Error en base de datos",E_USER_ERROR);
 	mysqli_set_charset($cnx, "utf8")
