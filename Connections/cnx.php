93ee19504993514581764ff7306b702b1d04da4e
oscarjoyablue <111010608+oscarjoyablue@users.noreply.github.com>
2023-08-06 18:40:45 -0600

    Borrado

--- Connections/cnx.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$hostname = 'bancoazulbd.database.windows.net';
-	$username = 'Administrador';
-	$password = 'Welcome.2020';
-	$database_cnx = 'elecciones2020';
-	$cnx = mysqli_connect($hostname, $username, $password, $database_cnx) or trigger_error("Error en base de datos",E_USER_ERROR);
-	mysqli_set_charset($cnx, "utf8")
-?>
--- includes/datatablesjs.php
+++
@@ -1,4 +0,0 @@
-<script src = "js/jquery-2.0.0.min.js" type="text/javascript"></script> 
-<script src = "js/bootstrap.min.js" type="text/javascript"></script> 
-<script src = "js/jquery.dataTables.min.js" type="text/javascript"></script> 
-<script src = "js/dataTables.bootstrap.min.js" type="text/javascript"></script>
